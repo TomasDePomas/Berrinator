d5ba90d15024e39508ecdb0baa15d6e466a4c1dd
thomasvanlankveld <thomas.van.lankveld@gmail.com>
2014-02-02 16:04:54 +0100

    replaced image functionality with a flexible berry picker

--- Berrinator.js
+++ Berrinator.js
@@ -21,27 +21,6 @@ function  BerryClass (classtag, nchild, wdomain){
 	this.wdomain = wdomain;
 }
 
-var getBerryWithIt = {
-	init: function(BerrinizeImage){
-		this.BerrinizeImage = BerrinizeImage;
-	},
-	horizontal: function(){
-		return this.BerrinizeImage.filter(function(BerrinizeImage){
-			return BerrinizeImage.ratio === 'horizontal';
-		});
-	},
-	vertical: function(){
-		return this.BerrinizeImage.filter(function(BerrinizeImage){
-			return BerrinizeImage.ratio === 'vertical';
-		});
-	},
-	square: function(){
-		return this.BerrinizeImage.filter(function(BerrinizeImage){
-			return BerrinizeImage.ratio === 'square';
-		});
-	}
-
-}
 var BerryClasses = [
 	new BerryClass('_52zl', false, 'facebook'),
 	new BerryClass('fwb', false, 'facebook'),
@@ -56,29 +35,151 @@ var BerryClasses = [
 
 ]
 
-var BerrinizeImage = [
-	new BerryImage("vertical", chrome.extension.getURL("/Berresources/berrie1.jpg")),
-	new BerryImage("square", chrome.extension.getURL("/Berresources/berrie2.jpg")),
-	new BerryImage("vertical", chrome.extension.getURL("/Berresources/berrie3.jpg")),
-	new BerryImage("horizontal", chrome.extension.getURL("/Berresources/berrie4.jpg")),
-
-	new BerryImage("square", "https://lh6.googleusercontent.com/-reG-zV02lS4/AAAAAAAAAAI/AAAAAAAAAPA/WvmTADt8oP4/photo.jpg"),
-	new BerryImage("vertical", "http://www.tue.nl/uploads/tx_dreep/19860175.jpg"),
-	new BerryImage("horizontal", "http://web.tue.nl/cursor/bastiaan/jaargang45/cursor27/rubrieken/images/berry%20eggen%20p2.jpg"),
+var BerryRatios = [
+	0.1,
+	0.125,
+	0.167,
+	0.2,
+	0.25,
+	0.286,
+	0.333,
+	0.4,
+	0.5,
+	0.571,
+	0.667,
+	0.8,
+	1,
+	1.25,
+	1.5,
+	1.75,
+	2,
+	2.5,
+	3,
+	3.5,
+	4,
+	4.5,
+	5,
+	6,
+	8,
+	10
 ]
 
-function imageRatio(image)
+var BerrinizeImage = [
+	new BerryImage(0.1, chrome.extension.getURL("/berryPics/0.100/0.100_Berry-Eggen.jpg")),
+
+	new BerryImage(0.125, chrome.extension.getURL("/berryPics/0.125/0.125_Berry-Eggen.jpg")),
+
+	new BerryImage(0.167, chrome.extension.getURL("/berryPics/0.167/0.167_Berry-Eggen.jpg")),
+
+	new BerryImage(0.2, chrome.extension.getURL("/berryPics/0.200/0.200_Berry-Eggen.jpg")),
+
+	new BerryImage(0.25, chrome.extension.getURL("/berryPics/0.250/0.250_Berry-Eggen.jpg")),
+
+	new BerryImage(0.286, chrome.extension.getURL("/berryPics/0.286/0.286_Berry-Eggen.jpg")),
+
+	new BerryImage(0.333, chrome.extension.getURL("/berryPics/0.333/0.333_Berry-Eggen.jpg")),
+	new BerryImage(0.333, chrome.extension.getURL("/berryPics/0.333/0.333_shapeimage_3_copy.png")),
+
+	new BerryImage(0.4, chrome.extension.getURL("/berryPics/0.400/0.400_Berry-Eggen.jpg")),
+	new BerryImage(0.4, chrome.extension.getURL("/berryPics/0.400/0.400_shapeimage_3_copy.png")),
+
+	new BerryImage(0.5, chrome.extension.getURL("/berryPics/0.500/0.500_Berry-Eggen.jpg")),
+	new BerryImage(0.5, chrome.extension.getURL("/berryPics/0.500/0.500_shapeimage_3_copy.png")),
+	new BerryImage(0.5, chrome.extension.getURL("/berryPics/0.500/0.500_4662436055_385433a1d4_o.jpg")),
+	new BerryImage(0.5, chrome.extension.getURL("/berryPics/0.500/0.500_4663056474_4d5c267d2e_o2.jpg")),
+
+	new BerryImage(0.571, chrome.extension.getURL("/berryPics/0.571/0.571_Berry-Eggen.jpg")),
+	new BerryImage(0.571, chrome.extension.getURL("/berryPics/0.571/0.571_shapeimage_3_copy.png")),
+	new BerryImage(0.571, chrome.extension.getURL("/berryPics/0.571/0.571_4662436055_385433a1d4_o.jpg")),
+	new BerryImage(0.571, chrome.extension.getURL("/berryPics/0.571/0.571_4663056474_4d5c267d2e_o2.jpg")),
+
+	new BerryImage(0.667, chrome.extension.getURL("/berryPics/0.667/0.667_Berry-Eggen.jpg")),
+	new BerryImage(0.667, chrome.extension.getURL("/berryPics/0.667/0.667_shapeimage_3_copy.png")),
+	new BerryImage(0.667, chrome.extension.getURL("/berryPics/0.667/0.667_4662436055_385433a1d4_o.jpg")),
+	new BerryImage(0.667, chrome.extension.getURL("/berryPics/0.667/0.667_4663056474_4d5c267d2e_o2.jpg")),
+
+	new BerryImage(0.8, chrome.extension.getURL("/berryPics/0.800/0.800_Berry-Eggen.jpg")),
+	new BerryImage(0.8, chrome.extension.getURL("/berryPics/0.800/0.800_shapeimage_3_copy.png")),
+	new BerryImage(0.8, chrome.extension.getURL("/berryPics/0.800/0.800_4662440051_4e30eb9b66_o.jpg")),
+	new BerryImage(0.8, chrome.extension.getURL("/berryPics/0.800/0.800_4663056474_4d5c267d2e_o2.jpg")),
+
+	new BerryImage(1, chrome.extension.getURL("/berryPics/1/1_Berry_Eggen.jpg")),
+	new BerryImage(1, chrome.extension.getURL("/berryPics/1/1_Berry-Eggen.jpg")),
+	new BerryImage(1, chrome.extension.getURL("/berryPics/1/1_4663056474_4d5c267d2e_o_copy.jpg")),
+	new BerryImage(1, chrome.extension.getURL("/berryPics/1/1_berry_eggen_p2.jpg")),
+	new BerryImage(1, chrome.extension.getURL("/berryPics/1/1_shapeimage_3_copy.png")),
+
+	new BerryImage(1.25, chrome.extension.getURL("/berryPics/1.25/1.25_berry_eggen_p2.jpg")),
+	new BerryImage(1.25, chrome.extension.getURL("/berryPics/1.25/1.25_Berry-Eggen.jpg")),
+	new BerryImage(1.25, chrome.extension.getURL("/berryPics/1.25/1.25_4663056474_4d5c267d2e_o2.jpg")),
+
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_Berry_Eggen.jpg")),
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_Berry-Eggen.jpg")),
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_616045_159797157495472_1472987606_o.jpg")),
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_4662440051_4e30eb9b66_o.jpg")),
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_4663056474_4d5c267d2e_o2.jpg")),
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_berry_eggen_p2.jpg")),
+	new BerryImage(1.50, chrome.extension.getURL("/berryPics/1.50/1.50_defense_05.jpg")),
+
+	new BerryImage(1.75, chrome.extension.getURL("/berryPics/1.75/1.75_Berry_Eggen.jpg")),
+	new BerryImage(1.75, chrome.extension.getURL("/berryPics/1.75/1.75_Berry-Eggen.jpg")),
+	new BerryImage(1.75, chrome.extension.getURL("/berryPics/1.75/1.75_616045_159797157495472_1472987606_o.jpg")),
+	new BerryImage(1.75, chrome.extension.getURL("/berryPics/1.75/1.75_4662440051_4e30eb9b66_o.jpg")),
+	new BerryImage(1.75, chrome.extension.getURL("/berryPics/1.75/1.75_4663056474_4d5c267d2e_o2.jpg")),
+	new BerryImage(1.75, chrome.extension.getURL("/berryPics/1.75/1.75_defense_05.jpg")),
+
+	new BerryImage(2, chrome.extension.getURL("/berryPics/2/2_4662440051_4e30eb9b66_o.jpg")),
+	new BerryImage(2, chrome.extension.getURL("/berryPics/2/2_Berry_Eggen.jpg")),
+	new BerryImage(2, chrome.extension.getURL("/berryPics/2/2_Berry-Eggen.jpg")),
+	new BerryImage(2, chrome.extension.getURL("/berryPics/2/2_defense_05.jpg")),
+
+	new BerryImage(2.5, chrome.extension.getURL("/berryPics/2.5/2.5_4662440051_4e30eb9b66_o.jpg")),
+	new BerryImage(2.5, chrome.extension.getURL("/berryPics/2.5/2.5_Berry_Eggen.jpg")),
+	new BerryImage(2.5, chrome.extension.getURL("/berryPics/2.5/2.5_defense_05.jpg")),
+
+	new BerryImage(3, chrome.extension.getURL("/berryPics/3/3_Berry_Eggen.jpg")),
+	new BerryImage(3, chrome.extension.getURL("/berryPics/3/3_defense_05.jpg")),
+	new BerryImage(3, chrome.extension.getURL("/berryPics/3/3_616045_159797157495472_1472987606_o.jpg")),
+
+	new BerryImage(3.5, chrome.extension.getURL("/berryPics/3.5/3.5_Berry_Eggen.jpg")),
+	new BerryImage(3.5, chrome.extension.getURL("/berryPics/3.5/3.5_defense_05.jpg")),
+
+	new BerryImage(4, chrome.extension.getURL("/berryPics/4/4_Berry_Eggen.jpg")),
+	new BerryImage(4, chrome.extension.getURL("/berryPics/4/4_Berry-Eggen.jpg")),
+	new BerryImage(4, chrome.extension.getURL("/berryPics/4/4_defense_05.jpg")),
+
+	new BerryImage(4.5, chrome.extension.getURL("/berryPics/4.5/4.5_Berry_Eggen.jpg")),
+	new BerryImage(4.5, chrome.extension.getURL("/berryPics/4.5/4.5_defense_05.jpg")),
+
+	new BerryImage(5, chrome.extension.getURL("/berryPics/5/5_Berry_Eggen.jpg")),
+	new BerryImage(5, chrome.extension.getURL("/berryPics/5/5_Berry-Eggen.jpg")),
+	new BerryImage(5, chrome.extension.getURL("/berryPics/5/5_defense_05.jpg")),
+
+	new BerryImage(6, chrome.extension.getURL("/berryPics/6/6_Berry_Eggen.jpg")),
+	new BerryImage(6, chrome.extension.getURL("/berryPics/6/6_Berry-Eggen.jpg")),
+	new BerryImage(6, chrome.extension.getURL("/berryPics/6/6_defense_05.jpg")),
+
+	new BerryImage(8, chrome.extension.getURL("/berryPics/8/8_Berry_Eggen.jpg")),
+	new BerryImage(8, chrome.extension.getURL("/berryPics/8/8_Berry-Eggen.jpg")),
+
+	new BerryImage(10, chrome.extension.getURL("/berryPics/10/10_Berry_Eggen.jpg")),
+	new BerryImage(10, chrome.extension.getURL("/berryPics/10/10_Berry-Eggen.jpg"))
+]
+
+function pickBerry(ratio)
 {
-	var proportion = image.height / image.width;
-	if(proportion > 1) {
-		return "vertical";
-	} else if(proportion === 1) {
-		return "square";
-	} else if(proportion < 1 ) {
-		return "horizontal";
-	}
-	return false;
+	for (var i = BerryRatios.length - 1; i >= 0; i--) {
+		if(ratio >= BerryRatios[i]) {
+			var sizedBerries = BerrinizeImage.filter(function(obj){
+				return obj.ratio == BerryRatios[i];
+			});
+
+			var chosenBerry = sizedBerries[Randomize(sizedBerries)];
+			return chosenBerry;
+		}
+	};
 }
+
 
 function Randomize (array){
 	return Math.floor(Math.random() * array.length);
@@ -133,12 +234,11 @@ function UnleashTheBerry() {
 			var orgHeight = images[i].height;
 			var orgWidth = images[i].width;
 
-			var ratio = imageRatio(images[i]);
+			var ratio = orgWidth / orgHeight;
 
 			if(ratio && (orgHeight > 22 && orgWidth > 22))
 			{
-				var number = Randomize(getBerryWithIt[ratio]());
-				var img = getBerryWithIt[ratio]()[number];
+				var img = pickBerry(ratio);
 
 				images[i].src = img.imageurl;
 				images[i].height = orgHeight;
@@ -162,7 +262,6 @@ $(document).ajaxComplete(function(){ console.log('vlam!')});
 	chrome.runtime.sendMessage({method: "getBerryAmount"}, function(response) {
 
 	  	BerryAmount = parseInt(response.status);
-		getBerryWithIt.init(BerrinizeImage);
 
 		UnleashTheBerry();
 
